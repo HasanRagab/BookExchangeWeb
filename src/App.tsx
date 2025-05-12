3a3bbc7d2e7271f6cde37111c4a712330f4ec243
2Hasan2 <h01554176846@gmail.com>
2025-05-12 18:20:16 +0300

    🔧 Refactor App.tsx component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
 import router from "./routes";
+import useAuthStore from "./store/authStore";
+import { useEffect } from "react";
+import { Loader2 } from "lucide-react";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  const { user, me, isLoading } = useAuthStore();
+  useEffect(() => {
+    if (!user) {
+      me();
+    }
+  }, [user, me]);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="animate-spin" />
+      </div>
+    );
+  }
+  return (
+    <>
+      <Toaster />
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
